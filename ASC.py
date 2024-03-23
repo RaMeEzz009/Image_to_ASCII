917780b0cff8d6bc8df91a85e118cc165be25518
RaMeEzz009 <rameez1614@gmail.com>
2024-03-23 16:19:39 +0530

    added comments

--- ASC.py
+++ ASC.py
@@ -1,8 +1,8 @@
 from PIL import Image
-
+#ASCII characters which will makeup the image
 ASCII_C=["@","#","$","%","?","!","*","+",";",":",",","."]
 
-
+# Resize function to change the dimensions of the image
 def resize(image,new_width=100):
     width,height=image.size
     ratio=height/width
@@ -10,18 +10,18 @@ def resize(image,new_width=100):
     resize_image=image.resize(((new_width+200),new_height))
     return resize_image
 
-
+# greyscale to change each pixel in the image to the its luminance
 def greyscale(image):
     new_image=image.convert("L")
     return new_image
 
-
+# this function will assign value to each pixel ranging from 0 to mumber fo character in the ASCII string
 def pixel_to_ASCII(image):
     pixels=image.getdata()
     charac="".join([ASCII_C[pixel//22] for pixel in pixels])
     return charac 
 
-    
+ # this is where we we will do the printing and inputing the file path   
 def main(new_width=300):
     path=input("Enter the path of the image:\n")
     try:
@@ -29,10 +29,9 @@ def main(new_width=300):
     except:
         print("Enter a valid path.\n")
     
+     
     
-    image = resize(image)
-    image = greyscale(image)
-    ascii_str = pixel_to_ASCII(image)
+    ascii_str = pixel_to_ASCII(greyscale(resize(image)))
         
     pixel_co=len(ascii_str)
     ascii_img="\n".join(ascii_str[i:(i+new_width)] for i in range(0,pixel_co,new_width))
